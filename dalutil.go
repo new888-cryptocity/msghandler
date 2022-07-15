fd65d45439ba9dc3036a0ffe065f44d4c5860ed6
new888 <103106029+new888-cryptocity@users.noreply.github.com>
2022-07-15 16:14:59 +0800

    Update dalutil.go

--- dalutil.go
+++ dalutil.go
@@ -4,7 +4,7 @@ import (
 	"666.com/gameserver/framework/db"
 	"666.com/gameserver/framework/mlog"
 	"666.com/gameserver/framework/msg"
-	"666.com/gameserver/netproto"
+	"github.com/new888-cryptocity/netproto"
 	"github.com/golang/protobuf/proto"
 )
 
